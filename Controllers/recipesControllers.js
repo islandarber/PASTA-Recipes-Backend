674d2433996de72e9fa22ac3b375ff2e48f346b8
Christina Vekri <christina.vek@gmail.com>
2024-02-02 17:59:36 +0100

    server done

--- Controllers/recipesControllers.js
+++ Controllers/recipesControllers.js
@@ -2,10 +2,28 @@ import {pool} from '../db/pool.js'
 
 export const getRecipes = async (req, res) => {
   try {
-    const {rows} = await pool.query('SELECT * FROM recipes;');
-    res.status(200).json(rows);
+    const {rows:recipesArray} = await pool.query('SELECT * FROM recipes;');
+    const { rows: ingredientsArray } = await pool.query('SELECT Recipes.id AS recipe_id, Recipes.name AS recipe_name, Ingredients.ingredient, RecipeIngredients.quantity FROM RecipeIngredients JOIN Ingredients ON RecipeIngredients.ingredient_id = Ingredients.id JOIN Recipes ON RecipeIngredients.recipe_id = Recipes.id');
+    const recipesWithIngredients = [];
+
+    recipesArray.forEach(recipe => {
+      const recipeIngredients = ingredientsArray
+          .filter(ingredient => ingredient.recipe_id === recipe.id)
+          .map(({ ingredient, quantity }) => `${quantity} ${ingredient}`)
+          .join(', ');
+  
+      const recipeWithIngredients = {
+          ...recipe,
+          ingredients: recipeIngredients,
+      };
+  
+      recipesWithIngredients.push(recipeWithIngredients);
+  });
+
+
+    res.status(200).json(recipesWithIngredients);
   } catch (error) {
-    res.status(500).send();
+    res.sendStatus(500);
     console.log(error);
   }
 }
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from 'cors';
 import 'dotenv/config';
 import recipesRouter from './Routes/recipes.js'
 
 const app = express();
+app.use(cors());
 const port = 8000;
 
 app.use(express.json());
